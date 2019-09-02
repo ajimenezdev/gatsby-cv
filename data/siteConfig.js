03567775c32a917214c5191d0601bce27b41c5f0
ajimenezdev <ajmjimens@gmail.com>
2019-09-02 17:54:40 +0200

    Update data

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,81 +1,138 @@
 module.exports = {
-  siteTitle: 'Hi! I\'m Francisco!',
+  siteTitle: "Hi! I'm Alvaro!",
   siteDescription: `Create your online curriculum in just a few minutes with this starter`,
   keyWords: ['gatsbyjs', 'react', 'curriculum'],
-  authorName: 'Francisco Santos',
-  twitterUsername: '_franciscodf',
-  githubUsername: 'santosfrancisco',
-  authorAvatar: '/images/avatar.jpeg',
-  authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
-    In 2012 I started working as a support technician and I approached the developers.
-    In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
-    I currently work as a frontend developer and mainly work with <strong>Javascript, NodeJS e React.</strong>`,
+  authorName: 'Alvaro Jimenez',
+  twitterUsername: 'ajimenezdev',
+  githubUsername: 'ajimenezdev',
+  authorAvatar: '/images/avatar.jpg',
+  authorDescription: `I'm a full-stack JS developer, currently focused in React/React Native development and blockchain solutions.<br/><br/>
+  I worked in multiple stacks (React, AngularJs, .Net, Java, Android...), environments and types of companies (Startup, medium size and big corporate).<br/><br/>
+  I currently work remotely as a frontend developer and mainly working with <strong>Javascript, React/ReactNative, NodeJS and Blockchain.</strong>`,
   skills: [
     {
       name: 'HTML',
-      level: 70
+      level: 80,
     },
     {
       name: 'CSS',
-      level: 60
+      level: 60,
     },
     {
       name: 'Javascript',
-      level: 50
-    },
-    {
-      name: 'NodeJs',
-      level: 40
+      level: 80,
     },
     {
       name: 'React',
-      level: 60
+      level: 75,
+    },
+    {
+      name: 'ReactNative',
+      level: 70,
+    },
+    {
+      name: 'NodeJs',
+      level: 40,
     },
     {
       name: 'Git',
-      level: 70
-    }
+      level: 70,
+    },
+    {
+      name: 'Bitcoin',
+      level: 30,
+    },
+    {
+      name: 'Ethereum & smartcontract',
+      level: 30,
+    },
   ],
   jobs: [
     {
-      company: "Lendico",
+      company: 'Lifelabs.io',
       begin: {
-        month: 'apr',
-        year: '2018'
+        month: 'may',
+        year: '2018',
       },
       duration: null,
-      occupation: "Frontend developer",
-      description: "I integrate the Frontend team responsible for developing and maintaining the online lending platform."
-  
-    }, {
-      company: "Anapro",
-      begin: {
-        month: 'dec',
-        year: '2016'
-      },
-      duration: '1 yr e 5 mos',
-      occupation: "Fullstack developer",
-      description: "Development and maintenance, corrective and preventive, of web applications for the real estate market."
-    }, {
-      company: "Anapro",
-      begin: {
-        month: 'set',
-        year: '2012'
-      },
-      duration: '4 yrs e 3 mos',
-      occupation: "Support Technician",
-      description: "Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool."
-  
+      occupation: 'Lead Frontend Developer',
+      description:
+        'I joined the company as one of the main developers, leading the frontend and mobile app development and helping with the architecture and coordination of the backend and blokchain teams.',
     },
-    /* ... */
+    {
+      company: 'Flipper',
+      begin: {
+        month: 'oct',
+        year: '2015',
+      },
+      duration: '2 years and 7 months',
+      occupation: 'Senior Developer',
+      description:
+        'I joined the company as one of the first 5 employees, I have been involved in all technical areas (backend, frontend, db, devops, bi) but with a higher responsibility on frontend, which I was leading inside the team',
+    },
+    {
+      company: 'Radisson Blu Edwardian London',
+      begin: {
+        month: 'oct',
+        year: '2013',
+      },
+      duration: '2 years',
+      occupation: 'Software developer',
+      description:
+        'Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool.',
+    },
+    {
+      company: 'HotelBeds',
+      begin: {
+        month: 'oct',
+        year: '2012',
+      },
+      duration: '1 years',
+      occupation: 'Software developer',
+      description:
+        'Member of a team in charge to create a new website to sell complementary holidays tickets and excursions, integrated inside the existing booking engine.',
+    },
+    {
+      company: 'Brujula',
+      begin: {
+        month: 'feb',
+        year: '2012',
+      },
+      duration: '7 months',
+      occupation: 'Junior developer',
+      description:
+        'Working in a team for a third party company as a development house, in charge of their intranet site to manage all the information about new customers, employees, keep the stock up to date and some other small applications.',
+    },
+    {
+      company: 'Bizzit',
+      begin: {
+        month: 'jul',
+        year: '2010',
+      },
+      duration: '3 months',
+      occupation: 'Intern developer',
+      description:
+        'Summer internship, Working with the company intranet site, creating new features and fixing some of the existing bugs.',
+    },
+    {
+      company: 'Bizzit',
+      begin: {
+        month: 'jul',
+        year: '2009',
+      },
+      duration: '3 months',
+      occupation: 'Intern developer',
+      description:
+        'Summer internship, Working with the company intranet site, creating new features and fixing some of the existing bugs.',
+    },
   ],
   social: {
-    twitter: "https://twitter.com/_franciscodf",
-    linkedin: "https://www.linkedin.com/in/santos-francisco",
-    github: "https://github.com/santosfrancisco",
-    email: "yoshi.df@gmail.com"
+    twitter: 'https://twitter.com/ajimenezdev/',
+    linkedin: 'https://www.linkedin.com/in/alvarojimenezmartin/',
+    github: 'https://github.com/ajimenezdev/',
+    email: 'ajmjimens@gmail.com',
   },
-  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+  siteUrl: 'https://alvarojimenezmartin.com',
   pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
   siteCover: '/images/cover.jpeg',
   googleAnalyticsId: 'UA-000000000-1',
@@ -85,8 +142,8 @@ module.exports = {
   icon: 'src/assets/gatsby-icon.png',
   headerLinks: [
     {
-      label: 'Francisco Santos',
+      label: 'Alvaro Jimenez Martin',
       url: '/',
-    }
-  ]
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Container, Row, Col } from 'react-awesome-styled-grid'
-import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter } from "react-icons/fa"
+import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter } from 'react-icons/fa'
 import siteConfig from '../../data/siteConfig'
 
 import Layout from '../components/layout'
@@ -19,57 +19,73 @@ const Separator = styled.hr`
 `
 
 class Home extends React.Component {
-  render () {
+  render() {
     // validate siteConfig settings
     if (siteConfig.googleAnalyticsId === 'UA-000000000-1') {
-      console.error('WARNING: Please set a proper googleAnalyticsId. See https://analytics.google.com for details.');
+      console.error(
+        'WARNING: Please set a proper googleAnalyticsId. See https://analytics.google.com for details.'
+      )
     }
 
     const title = siteConfig.siteTitle
-    const {keywords} = siteConfig
+    const { keywords } = siteConfig
     return (
       <Layout location={this.props.location}>
-        <SEO
-          title={title}
-          keywords={keywords}
-        />
+        <SEO title={title} keywords={keywords} />
 
-        <Hero
-          heroImg={siteConfig.siteCover}
-          title={title}
-        />
+        <Hero heroImg={siteConfig.siteCover} title={title} />
 
-        <Wrapper className={this.props.className} >
+        <Wrapper className={this.props.className}>
           <Container className="page-content" fluid>
             <Row>
-              <Col xs={4} className='avatar'>
+              <Col xs={4} className="avatar">
                 <img
-                  className='avatar__image'
-                  src='/images/avatar.jpeg'
-                  alt='user avatar'
+                  className="avatar__image"
+                  src="/images/avatar.jpg"
+                  alt="user avatar"
                 />
                 <div className="social">
-                  {siteConfig.social.github && <a className="social-link github" href={siteConfig.social.github}>
-                    <FaGithub className="social-icon" size="32" />
-                  </a>}
-                  {siteConfig.social.linkedin && <a className="social-link linkedin" href={siteConfig.social.linkedin}>
-                    <FaLinkedin className="social-icon" size="32" />
-                  </a>}
-                  {siteConfig.social.twitter && <a className="social-link twitter" href={siteConfig.social.twitter}>
-                    <FaTwitter className="social-icon" size="32" />
-                  </a>}
-                  {siteConfig.social.email && <a className="social-link email" href={`mailto:${siteConfig.social.email}`}>
-                    <FaEnvelope className="social-icon" size="32" />
-                  </a>}
+                  {siteConfig.social.github && (
+                    <a
+                      className="social-link github"
+                      href={siteConfig.social.github}
+                    >
+                      <FaGithub className="social-icon" size="32" />
+                    </a>
+                  )}
+                  {siteConfig.social.linkedin && (
+                    <a
+                      className="social-link linkedin"
+                      href={siteConfig.social.linkedin}
+                    >
+                      <FaLinkedin className="social-icon" size="32" />
+                    </a>
+                  )}
+                  {siteConfig.social.twitter && (
+                    <a
+                      className="social-link twitter"
+                      href={siteConfig.social.twitter}
+                    >
+                      <FaTwitter className="social-icon" size="32" />
+                    </a>
+                  )}
+                  {siteConfig.social.email && (
+                    <a
+                      className="social-link email"
+                      href={`mailto:${siteConfig.social.email}`}
+                    >
+                      <FaEnvelope className="social-icon" size="32" />
+                    </a>
+                  )}
                 </div>
               </Col>
             </Row>
             <Row>
               <Col xs={4} sm={4}>
-                <About title='About' text={siteConfig.authorDescription}/>
+                <About title="About" text={siteConfig.authorDescription} />
               </Col>
               <Col xs={4} sm={4}>
-                <Skills title='Skills' skills={siteConfig.skills} />
+                <Skills title="Skills" skills={siteConfig.skills} />
               </Col>
             </Row>
             <Separator />
@@ -91,11 +107,11 @@ export default styled(Home)`
 
   .avatar {
     align-items: center;
-  margin-bottom: 24px;
+    margin-bottom: 24px;
   }
 
   .avatar__image {
-    box-shadow: 3px 3px 15px 0px rgba(0,0,0,0.75);
+    box-shadow: 3px 3px 15px 0px rgba(0, 0, 0, 0.75);
     max-width: 200px;
     border-radius: 100px;
     margin: 0 auto 24px;
@@ -120,7 +136,7 @@ export default styled(Home)`
   }
 
   a.social-link.linkedin:hover {
-    color: #0077B5;
+    color: #0077b5;
   }
 
   a.social-link.email:hover {
